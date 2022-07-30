cbe810a292238cacd00f2237ccec3ef44eed335e
Gowtham <gowtham180502@gmail.com>
2022-07-30 12:55:06 +0530

    YouTube video/audio downloader

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def checkingInternet(url='https://pynerds.blogspot.com/'):
 # downloads the file
 def downlaod(format, video_id, extension):
     op_path = f"{os.path.expanduser('~')}/Downloads"
+    print(op_path)
     format.download(output_path=op_path, filename=f"{video_id}.{extension}")
 
 with header:
